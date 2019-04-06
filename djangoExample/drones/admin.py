0afb5dd5ecd9067a771267969b61d496548cf08d
Digindataflow <31276943+Digindataflow@users.noreply.github.com>
2019-04-06 11:15:57 +0200

    new admin

--- djangoExample/drones/admin.py
+++ djangoExample/drones/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from drones.models import Drone, DroneCategory, Pilot, Competition
 
 # Register your models here.
+admin.site.register(Drone)
+admin.site.register(DroneCategory)
+admin.site.register(Pilot)
+admin.site.register(Competition)
